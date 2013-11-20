86a16b0e3698b0bd3d7dd46b9bee2ee8adb17506
jrojas <jorge.l.rojas@gmail.com>
2013-11-19 16:17:28 -0800

    Hover

---
+++ js/application.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+  // Handler for .ready() called.
+  $('.content-blocks').hide();
+  $('.inner-blocks').mouseenter(function(){
+   
+	
+  
+  });
+  
+  $('.inner-blocks').mouseleave(function(){
+    
+  });
+});
